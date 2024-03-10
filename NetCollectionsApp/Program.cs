8ed44bd687e2a3152be875d32802c256be62ad86
maxefimov <efimov.top.academy@gmail.com>
2024-03-10 13:15:50 +0300

    Добавьте файлы проекта.

---
+++ NetCollectionsApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+
+ArrayList arrayList = new ArrayList();
+Random random = new Random();
+
+int size = 10;
+
+for (int i = 0; i < size; i++)
+    arrayList.Add(random.Next(99));
+
+//arrayList[3] = 656.8787;
+//arrayList[5] = "Hello world";
+//arrayList[7] = false;
+
+foreach(var item in arrayList)
+    Console.Write($"{item} ");
+Console.WriteLine();
